68a93f24f56d46e892769fd6cc90d1dbbddad35b
Abhijeet <pabhijeet023@gmail.com>
2022-09-21 21:38:51 +0530

    first commit

---
+++ Abc.java
@@ -0,0 +1,32 @@
+
+public class Abc{
+
+public static void main(String [] args){
+
+for(int row =1; row<=5; row++){
+
+	for ( int column=1; column<=5; column++){
+		if(column==1||column==5|| column == row){
+    System.out.print("*");
+
+					     }
+else{
+  System.out.print(" ");
+    }
+
+						}
+System.out.println();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+}
